7319c12db7a559f08558f5047af54dd77b3aa0ab
MuneebMoosa <muneebmoosavv@gmail.com>
2026-06-14 00:25:40 +0530

    feat: add booking UI

---
+++ client/src/pages/venueDetails/venueData.ts
@@ -0,0 +1,179 @@
+export const venueData = {
+  id: 1,
+  venueName: "Royal Palace Hall",
+  venueType: "Hall",
+  district: "Thrissur",
+  city: "Kodungallur",
+  description:
+    "A premium wedding and event hall with AC, parking and dining facilities.",
+  maxCapacity: 500,
+
+  images: [
+    "https://images.unsplash.com/photo-1519167758481-83f550bb49b3",
+    "https://images.unsplash.com/photo-1511578314322-379afb476865",
+    "https://images.unsplash.com/photo-1519167758481-83f550bb49b3",
+    "https://images.unsplash.com/photo-1511578314322-379afb476865",
+    "https://images.unsplash.com/photo-1519167758481-83f550bb49b3",
+    "https://images.unsplash.com/photo-1511578314322-379afb476865",
+  ],
+
+  amenities: [
+    "Parking",
+    "AC",
+    "Wifi",
+    "Dining Area",
+    "Parking",
+    "AC",
+    "Wifi",
+    "Dining Area",
+    "AC",
+    "Wifi",
+    "Dining Area",
+  ],
+
+  spaceAttributes: [
+    "Indoor",
+    "Poolside",
+  ],
+
+  seatingConfigurations: [
+    "Banquet Seating",
+    "Theatre Seating",
+  ],
+
+  fullAddress:
+    "Kodungallur Road, Thrissur",
+  pin:
+    679000,
+
+  googleMapsLink:
+    "https://maps.app.goo.gl/fHpKfrWovqL5JyCn7",
+
+  bookingType: "fixed",
+  // bookingType: "flexible",
+
+  fixedSlots: [
+  {
+    slotName: "Morning",
+    startTime: "08:00",
+    endTime: "14:00",
+    price: 20000
+  },
+
+  {
+    slotName: "Evening",
+    startTime: "16:00",
+    endTime: "22:00",
+    price: 25000
+  },
+  {
+    slotName: "Morning",
+    startTime: "08:00",
+    endTime: "14:00",
+    price: 20000
+  },
+
+  {
+    slotName: "Evening",
+    startTime: "16:00",
+    endTime: "22:00",
+    price: 25000
+  },
+  {
+    slotName: "Morning",
+    startTime: "08:00",
+    endTime: "14:00",
+    price: 20000
+  },
+
+  {
+    slotName: "Evening",
+    startTime: "16:00",
+    endTime: "22:00",
+    price: 25000
+  },
+  {
+    slotName: "Morning",
+    startTime: "08:00",
+    endTime: "14:00",
+    price: 20000
+  },
+
+  {
+    slotName: "Evening",
+    startTime: "16:00",
+    endTime: "22:00",
+    price: 25000
+  },
+],
+
+  generatedSlots: [
+  {
+    startTime: "09:00 AM",
+    endTime: "10:00 AM",
+    price: 1000,
+  },
+
+  {
+    startTime: "10:00 AM",
+    endTime: "11:00 AM",
+    price: 1000,
+  },
+
+  {
+    startTime: "11:00 AM",
+    endTime: "12:00 PM",
+    price: 1000,
+  },
+
+  {
+    startTime: "12:00 PM",
+    endTime: "01:00 PM",
+    price: 1200,
+  },
+
+  {
+    startTime: "01:00 PM",
+    endTime: "02:00 PM",
+    price: 1200,
+  },
+
+  {
+    startTime: "02:00 PM",
+    endTime: "03:00 PM",
+    price: 1200,
+  },
+
+  {
+    startTime: "05:00 PM",
+    endTime: "06:00 PM",
+    price: 2000,
+  },
+
+  {
+    startTime: "06:00 PM",
+    endTime: "07:00 PM",
+    price: 2000,
+  },
+
+  {
+    startTime: "07:00 PM",
+    endTime: "08:00 PM",
+    price: 2000,
+  },
+
+  {
+    startTime: "08:00 PM",
+    endTime: "09:00 PM",
+    price: 2500,
+  },
+
+  {
+    startTime: "09:00 PM",
+    endTime: "10:00 PM",
+    price: 2500,
+  },
+],
+
+pricePerHour: 2000,
+}
