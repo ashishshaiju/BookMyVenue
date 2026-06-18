d2c09ef6ec7b87bb7babc298cfef402f2b05863b
ashishshaiju <74810128+ashishshaiju@users.noreply.github.com>
2026-06-18 15:55:10 +0530

    chore(dbutils): simplify transaction wrapper to return directly

--- server/src/utils/dbUtils.ts
+++ server/src/utils/dbUtils.ts
@@ -1,18 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export async function runInTransaction<T>(
   work: (session: mongoose.ClientSession) => Promise<T>
 ): Promise<T> {
   const session = await mongoose.startSession();
   try {
-    let result: T | undefined;
-    await session.withTransaction(async () => {
-      result = await work(session);
-    });
-    if (result === undefined) {
-      throw new Error("Transaction did not yield a result");
-    }
-    return result;
+    return await session.withTransaction(() => work(session));
   } finally {
     await session.endSession();
   }
